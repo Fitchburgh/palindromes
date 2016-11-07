5dada7deeddf88dcc2c87954c53c0e9db04f5851
Brandon Fitch <fitchbj@gmail.com>
2016-11-07 14:29:23 -0500

    Fixed adv mode

--- palindrome_adv.rb
+++ palindrome_adv.rb
@@ -1,42 +1,21 @@
-def is_palindrome?()
-  user_string = self.downcase.gsub(/[^0-9A-Za-z]/ ,"").scan(/./)
+require 'pry'
+def palindrome?(user_string)
+  user_string = user_string.downcase.gsub(/[^0-9A-Za-z]/, '').scan(/./)
   if user_string.length <= 1
     true
+  elsif user_string.pop == user_string.shift
+    palindrome?(user_string.join)
   else
-    if user_string.pop == user_string.shift
-      user_string.join.is_palindrome?
-    else
-      false
-    end
+    false
   end
 end
 
-print": "
-input = gets.downcase.gsub(/[^0-9A-Za-z]/ ,"")
-is_palindrome?()
+print ': '
+input = gets.downcase.gsub(/[^0-9A-Za-z]/, '')
+answer = palindrome?(input)
 
-if input == []
-  puts "You got 'r!"
+if answer == true
+  puts 'You got \'r!'
 else
-  puts "Dis not no 'drome!"
+  puts 'Dis not no \'drome!'
 end
-
-
-
-
-
-
-
-
-
-
-
-# string.split("")
-# use .pop and .shift to remove first and last of an array
-
-# user_file = 'palindrome.txt'
-# f = File.open(user_file, "r")
-# f.each_line { |line|
-#   puts line
-# }
-# f.close
--- palindrome_epic.rb
+++ palindrome_epic.rb
@@ -1,10 +1,9 @@
-#Why was I not able to use class Array or class String
-def is_palindrome?(input)
-    input.each do |letter|
+def palindrome?(input)
+  input.each do |letter|
     if input.first == input.last
       input.pop
       input.shift
-      is_palindrome?(input)
+      palindrome?(input)
     end
   end
 end
@@ -12,14 +11,12 @@ end
 command_line_input = ARGV.first
 
 File.foreach(command_line_input) do |line|
-    puts line
-    line = gets.downcase.gsub(/[^0-9A-Za-z]/ ,"").scan(/./)
-    is_palindrome?(line)
-    if line == []
-      puts "Dis a 'drome!
-      "
-    else
-      puts "Dis ain't no 'drome!
-      "
-    end
+  puts line
+  line = gets.downcase.gsub(/[^0-9A-Za-z]/, '').scan(/./)
+
+  if palindrome?(line)
+    puts "Dis a 'drome!"
+  else
+    puts "Dis ain't no 'drome!"
   end
+end
